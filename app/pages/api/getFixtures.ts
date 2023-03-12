f8bf2dd9fec4d344e20ed671c4241469cdfbf573
Barto Molina <bartomolina@gmail.com>
2023-03-12 16:29:45 +0100

    Cache first load fix

--- app/pages/api/getFixtures.ts
+++ app/pages/api/getFixtures.ts
@@ -32,6 +32,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         console.log("No cached data found");
       }
     }
+  } else {
+    refreshCache = true;
   }
 
   if (refreshCache) {
