0e223f06c8987b574d4479d23db213377b853942
Barto Molina <bartomolina@gmail.com>
2023-03-13 19:08:22 +0100

    Updated screenshot location

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -29,7 +29,7 @@ const About = () => (
         <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
           <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-2">
             <Image
-              src="/screenshot3.png"
+              src="/screenshot.png"
               alt="ScoreCast screenshot"
               width={2432}
               height={1442}
